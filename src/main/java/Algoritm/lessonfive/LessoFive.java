840e67c06e4e71d47f1e50fc529f72579fe1f1a9
IrinaNaumenko <irina1280808@gmail.com>
2022-08-28 11:20:55 +0200

    Add new lesson add Privet

--- src/main/java/Algoritm/lessonfive/LessoFive.java
+++ src/main/java/Algoritm/lessonfive/LessoFive.java
@@ -3,5 +3,7 @@ package Algoritm.lessonfive;
 public class LessoFive {
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("Privet");
+
     }
 }
